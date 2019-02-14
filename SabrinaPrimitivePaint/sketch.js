1f13bf52e3771a7ce9c35ea46031a5b81056d3fc
sabrinak28 <47329876+sabrinak28@users.noreply.github.com>
2019-02-14 10:36:25 -0600

    Update sketch.js

--- SabrinaPrimitivePaint/sketch.js
+++ SabrinaPrimitivePaint/sketch.js
@@ -32,65 +32,67 @@
 // }
 //
 
-let img;
-let imgSize;
-let imgArray = [];
+let shape;
+let choice;
+let shapeSize;
+let rectArray = [];
+let ellipseArray = [];
+let triangleArray = [];
 
-
-function preload(){
-  if (img === 1){
-    rect(mouseX, mouseY, 50, 50);
-  }
-  if (img === 2){
-    ellipse(mouseX, mouseY, 50, 50);
-  }
-  if (img === 3){
-    triangle(mouseX, mouseY, mouseX, (mouseY + 50), (mouseX + 50), mouseY);
-  }
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  imageMode(CENTER);
-  imgSize = 200;
+
+  shapeSize = 200;
 }
 
 function draw() {
   background(220);
-  for (let i = 0; i < imgArray.length; i++){
-    image(img, imgArray[i][0], imgArray [i][1], imgArray[i][2], imgArray [i][2])
+  for (let i = 0; i < shapeArray.length; i++){
+    rect(shape, shapeArray[i][0], shapeArray [i][1], shapeArray[i][2], shapeArray [i][2])
   }
-  image(img, mouseX, mouseY, imgSize, imgSize);
-}
+
+  for (let i = 0; i < shapeArray.length; i++){
+    ellipse(shape, shapeArray[i][0], shapeArray [i][1], shapeArray[i][2], shapeArray [i][2])
+  }
+  for (let i = 0; i < shapeArray.length; i++){
+    triangle(shape, shapeArray[i][0], shapeArray [i][1], shapeArray[i][2], shapeArray [i][2])
+  }
+  rect(mouseX, mouseY, 50, 50);
+  ellipse(mouseX, mouseY, 50, 50);
+  triangle(mouseX, mouseY, mouseX, (mouseY + 50), (mouseX + 50), mouseY);
+
 
 function keyTyped(){
   if (key === 'a'){
-    img = 1;
-    fill(random(255), random(255), random(255));
+    shape = rect;
+    rectArray.push(cur);
+
   }
   if (key === 's'){
-    img = 2;
-    fill(random(255), random(255), random(255));
+    shape = ellipse(mouseX, mouseY, 50, 50);
+    choice = 2;
+
   }
   if (key === 'd'){
-    img = 3;
-    fill(random(255), random(255), random(255));
+    shape = triangle(mouseX, mouseY, mouseX, (mouseY + 50), (mouseX + 50), mouseY);
+    choice = 3;
   }
 }
 
 function mousePressed(){
-  let cur = [mouseX, mouseY, imgSize];
-  imgArray.push(cur);
+  let cur = [mouseX, mouseY, shapeSize];
+  rectArray.push(cur);
 }
 
-function mouseWheel(event){
-  if (event.delta > 0){
-    imgSize += 25;
-  }
-  else{
-    if(imgSize > 25){
-      imgSize -= 25;
-    }
-  }
-  return false;
-}
+// function mouseWheel(event){
+//   if (event.delta > 0){
+//     imgSize += 25;
+//   }
+//   else{
+//     if(imgSize > 25){
+//       imgSize -= 25;
+//     }
+//   }
+//   return false;
+// }
