0ef5453a55e1e744115aed050f9db1b70b531705
sabrinak28 <47329876+sabrinak28@users.noreply.github.com>
2019-02-15 11:03:26 -0600

    Update sketch.js

--- SabrinaPrimitivePaint/sketch.js
+++ SabrinaPrimitivePaint/sketch.js
@@ -5,94 +5,32 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-//   background(220);
-// }
-
-// function keyTyped(){
-//   if (key === 'a'){
-//     fill(random(255), random(255), random(255));
-//     rect(mouseX, mouseY, 50, 50);
-//   }
-//   if (key === 's'){
-//     fill(random(255), random(255), random(255));
-//     ellipse(mouseX, mouseY, 50, 50);
-//   }
-//   if (key === 'd'){
-//     fill(random(255), random(255), random(255));
-//     triangle(mouseX, mouseY, mouseX, (mouseY + 50), (mouseX + 50), mouseY);
-//   }
-// }
-
-// function keyPressed(){
-//   if (keycode === 'c'){
-//     fill(random(255), random(255), random(255));
-//   }
-// }
-//
-
-let shape;
-let choice;
-let shapeSize;
-let rectArray = [];
-let ellipseArray = [];
-let triangleArray = [];
-
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
+  textFont("Curlz MT");
+  textSize(width / 10);
+  textAlign(CENTER);
+  fill(255);
+  text("Sabrina Kettle", width / 2, height / 2);
 
-  shapeSize = 200;
 }
-
-function draw() {
-  background(220);
-  for (let i = 0; i < shapeArray.length; i++){
-    rect(shape, shapeArray[i][0], shapeArray [i][1], shapeArray[i][2], shapeArray [i][2])
-  }
-
-  for (let i = 0; i < shapeArray.length; i++){
-    ellipse(shape, shapeArray[i][0], shapeArray [i][1], shapeArray[i][2], shapeArray [i][2])
-  }
-  for (let i = 0; i < shapeArray.length; i++){
-    triangle(shape, shapeArray[i][0], shapeArray [i][1], shapeArray[i][2], shapeArray [i][2])
-  }
-  rect(mouseX, mouseY, 50, 50);
-  ellipse(mouseX, mouseY, 50, 50);
-  triangle(mouseX, mouseY, mouseX, (mouseY + 50), (mouseX + 50), mouseY);
-
 
 function keyTyped(){
   if (key === 'a'){
-    shape = rect;
-    rectArray.push(cur);
-
+    fill(random(255), random(255), random(255));
+    rect(mouseX, mouseY, random(300), random(300));
   }
   if (key === 's'){
-    shape = ellipse(mouseX, mouseY, 50, 50);
-    choice = 2;
-
+    fill(random(255), random(255), random(255));
+    ellipse(mouseX, mouseY, random(300), random(300));
   }
   if (key === 'd'){
-    shape = triangle(mouseX, mouseY, mouseX, (mouseY + 50), (mouseX + 50), mouseY);
-    choice = 3;
+    fill(random(255), random(255), random(255));
+    triangle(mouseX, mouseY, mouseX, (mouseY + random(300)), (mouseX + random(300)), mouseY);
+  }
+  if (key === " "){
+    background(0);
+    text("Sabrina Kettle", width / 2, height / 2);
   }
 }
-
-function mousePressed(){
-  let cur = [mouseX, mouseY, shapeSize];
-  rectArray.push(cur);
-}
-
-// function mouseWheel(event){
-//   if (event.delta > 0){
-//     imgSize += 25;
-//   }
-//   else{
-//     if(imgSize > 25){
-//       imgSize -= 25;
-//     }
-//   }
-//   return false;
-// }
