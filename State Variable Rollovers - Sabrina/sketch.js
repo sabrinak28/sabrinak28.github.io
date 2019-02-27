5daba9bb602cc3a142747a1eb72da3d1c14e59c8
sabrinak28 <47329876+sabrinak28@users.noreply.github.com>
2019-02-27 11:06:08 -0600

    Update sketch.js

--- State Variable Rollovers - Sabrina/sketch.js
+++ State Variable Rollovers - Sabrina/sketch.js
@@ -7,55 +7,95 @@
 
 
 let quad;
-let colourI =  0;
-let colourII, colourIII, colourIV;
+let colourI = 255;
+let colourII = 255;
+let colourIII = 255;
+let colourIV = 255;
+let hoverII = false;
+let hoverIV = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-  background(255);
+function determineQuad(){
+  if (mouseX > width/2 && mouseX < width && mouseY > 0 && mouseY < height/2){
+    quad = 1;
+  }
 
-  //Fill quadrant II
   if (mouseX > 0 && mouseX < width/2 && mouseY > 0 && mouseY < height/2){
-    fill(0);
+    quad = 2;
   }
-  else{
-    fill(255);
-  }
-  rect(0, 0, width/2, height/2);
 
+  if (mouseX > 0 && mouseX < width/2 && mouseY > height/2 && mouseY < height){
+    quad = 3;
+  }
+
+  if (mouseX > width/2 && mouseX < width && mouseY > height/2 && mouseY < height){
+    quad = 4;
+  }
+}
+
+function fillQuad(){
+  if (quad === 1){
+    fill(colourI);
+    rect(width/2, 0, width/2, height/2);
+  }
+}
+
+function draw() {
+
+  background(255);
+  determineQuad();
 
   //Fill Quadrant I
-  if (mouseX > width/2 && mouseX < width && mouseY > 0 && mouseY < height/2){
+  if (quad === 1){
     colourI = 0;
-    fill(colourI);
   }
   else{
     colourI += 5;
   }
   colourI = constrain(colourI, 0, 255);
-  fill(colourI);
-  rect(width/2, 0, width/2, height/2);
+  fillQuad();
 
+  //Fill quadrant II
+  if (quad === 2){
+    hoverII = true;
+    colourII = 0;
+  }
+  else{
+    hoverII = false;
+    colourII += 5;
+  }
+  colourII = constrain(colourII, 0, 255);
+  fill(colourII);
+  rect(0, 0, width/2, height/2);
+  
 
   //Fill Quadrant III
-  if (mouseX > 0 && mouseX < width/2 && mouseY > height/2 && mouseY < height){
-    fill(0);
+  if (quad === 3){
+    colourIII = 0;
   }
   else{
-    fill(255);
+    colourIII += 5;
   }
+  colourIII = constrain(colourIII, 0, 255);
+  fill(colourIII);
   rect(0, height/2, width/2, height/2);
 
+
   //Fill Quadrant IV
-  if (mouseX > width/2 && mouseX < width && mouseY > height/2 && mouseY < height){
-    fill(0);
-    fadeOut = false;
+  if (quad === 4){
+    hoverIV = true;
+    colourIV = 0;
   }
   else{
-    fill(255);
+    hoverIV = false;
+    colourIV += 5;
   }
+  colourIV = constrain(colourIV, 0, 255);
+  fill(colourIV);
   rect(width/2, height/2, width/2, height/2);
+
+
 }
