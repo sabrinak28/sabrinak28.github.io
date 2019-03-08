05e679376243080b86333f5f3b90829c03e56da4
sabrinak28 <47329876+sabrinak28@users.noreply.github.com>
2019-03-08 11:02:04 -0600

    Update sketch.js

--- Multi-Coloured Grid/sketch.js
+++ Multi-Coloured Grid/sketch.js
@@ -7,6 +7,9 @@
 
 let sNumber = 20;
 let sRed, sBlue, sGreen;
+let count = true;
+let scheme = 0;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,31 +17,64 @@ function setup() {
 }
 
 function draw() {
-  makeSquares();
-}
-
-function makeSquares(){
+  if (count){
+    makeSquares();
+  } 
   if (mouseIsPressed){
     if (mouseButton === RIGHT){
       sNumber =- 1;
+      count = true;
     }
     else if (mouseButton === LEFT){
       sNumber =+ 1;
+      count = true;
     }
   }
-  let squareSize = width/sNumber;
+}
 
-  for (let x = 0; x < width; x+= squareSize){
-    for (let y = 0; y < height; y+= squareSize){
+function makeSquares(){
+  let squareSize = sNumber;
+  for (let x = 0; x <= width; x+= squareSize){
+    for (let y = 0; y <= height; y+= squareSize){
       makeColours();
       fill(sRed, sGreen, sBlue);
       rect(x, y, squareSize, squareSize);
     }
   }
+  count = false;
 }
 
 function makeColours(){
-  sRed = int(random(255));
-  sBlue = int(random(255));
-  sGreen = int(random(255));
+  if (scheme === 1){
+    sRed = int(random(0,50));
+    sBlue = int(random(75,255));
+    sGreen = int(random(0,50));
+  }
+  else if (scheme === 0){
+    sRed = int(random(255));
+    sBlue = int(random(255));
+    sGreen = int(random(255));
+  }
+}
+
+// function mousePressed(){
+//   if (mouseButton === RIGHT){
+//     sNumber =- 1;
+//     count = true;
+//   }
+//   else if (mouseButton === LEFT){
+//     sNumber =+ 1;
+//     count = true;
+//   }
+// }
+
+function keyPressed(){
+  if (key === " "){
+    scheme = 0;
+    count = true;
+  }
+  if (key === "b"){
+    scheme = 1;
+    count = true;
+  }
 }
