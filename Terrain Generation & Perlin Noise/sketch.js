fd86f618affe3ea1d268d22beae12ad4a9661d09
sabrinak28 <47329876+sabrinak28@users.noreply.github.com>
2019-03-18 10:03:54 -0600

    Whaaaaat

--- Terrain Generation & Perlin Noise/sketch.js
+++ Terrain Generation & Perlin Noise/sketch.js
@@ -5,34 +5,6 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// let rectSize = 5;
-// let xoff = -500;
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-//   fill(100);
-//   generateTerrain();
-// }
-
-// function draw() {
-// }
-
-// function generateTerrain(){
-//   for (let c = 0; c < width; c += rectSize){
-//     //rect(x, height, rectSize, random(-20, -500));
-//     let x = map(noise(xoff), 0 , 1, 0, width);
-//     xoff -= 0.001;
-//     rect(c, height, rectSize, noise(xoff));
-//   }
-// }
-
-// function keyPressed(){
-//   if (key === " "){
-//     background(255);
-//     generateTerrain();
-//   }
-// }
-
 let tWidth = 1;
 let start = 0;
 
@@ -66,3 +38,5 @@ function draw(){
   background(220);
   generateTerrain();
 }
+
+//Average = height - currentHeight
