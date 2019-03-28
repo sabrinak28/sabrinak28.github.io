fb3833a2167c60386eb11ca7bc310b282106972c
sabrinak28 <47329876+sabrinak28@users.noreply.github.com>
2019-03-28 11:06:19 -0600

    boop

--- Refactoring Excercises/Refactoring Excercise One/sketch.js
+++ Refactoring Excercises/Refactoring Excercise One/sketch.js
@@ -1,15 +1,47 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Refactoring Excercise One
+// Sabrina Kettle
+// March 28th, 2019
 
+//Variables:-----------------------
+
+let rectX, rectY, xSpeed, ySpeed;
+
+//---------------------------------
+
+//Setup:-------------------------------------------------------------------
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectX = 200; rectY = 300; xSpeed = random(3, 8); ySpeed = random(3, 8);
 }
 
-function draw() {
-  background(220);
+//-------------------------------------------------------------------------
+
+//This function will constantly move the rectangle:---
+
+function rectPosition(){
+
+  rectX += xSpeed; //Change X position
+  rectY += ySpeed; //Change Y position
+
+  if (rectX >= width - 250 || rectX <= 0){ //Restrict
+    xSpeed = xSpeed * -1;
+  }
+
+  if (rectY >= height - 75 || rectY <= 0){ //Restrict
+    ySpeed = ySpeed * -1;
+  }
+
 }
+
+//----------------------------------------------------
+
+//Draw:-----------------------------------------------
+
+function draw() {
+  background(80, 80, 80);
+  rectPosition(); //Get rectangle information
+  rect(rectX, rectY, 250, 75); //Build rectangle
+}
+
+//----------------------------------------------------
--- Refactoring Excercises/Refactoring Excercise Two/sketch.js
+++ Refactoring Excercises/Refactoring Excercise Two/sketch.js
@@ -1,15 +1,70 @@
-// Project Title
-// Your Name
-// Date
+// Refactoring Excercise Two
+// Sabrina Kettle
+// March 28th, 2019
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// This example is adapted from Learning Processing Example 5-3 by Daniel Shiffman
+// http://www.learningprocessing.com
+// Refactor the following code. Be sure the refactored version:
+//  - is readable
+//  - is able to work easily with any canvas size
 
+//Variables:----
+
+let rectX;
+let rectY;
+
+//--------------
+
+//Setup:---------------------
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(480, 270);
+
+  //Variable Assignment
+  rectX = width/2;
+  rectY = height/2;
 }
 
-function draw() {
-  background(220);
+//---------------------------
+
+//This will make the graph in the background:-------
+
+function makeGraph(){
+  stroke(0);
+  line(rectX, 0, rectX, height); //Vertical axis
+  line(0, rectY, width, rectY); //Horizontal axis
 }
+
+//--------------------------------------------------
+
+//When hovering over a rectangle, it will fill back:----
+
+function fillGraph(){
+  noStroke(); //Set up fill
+  fill(0);
+
+  if (mouseX > rectX && mouseY < rectY){
+    rect(rectX, 0, rectX, rectY); //Fill Quad I
+  }
+  else if (mouseX < rectX && mouseY < rectY){
+    rect(0, 0, rectX, rectY); //Fill Quad II
+  }
+  else if (mouseX < rectX && mouseY > rectY){
+    rect(0, rectY, rectX, rectY); //Fill Quad III
+  }
+  else if (mouseX > rectX && mouseY > rectY){
+    rect(rectX, rectY, rectX, rectY); //Fill Quad IV
+  }
+}
+
+//------------------------------------------------------
+
+//Draw:---------------
+
+function draw() {
+  background(255);
+  makeGraph();
+  fillGraph();
+}
+
+//--------------------
