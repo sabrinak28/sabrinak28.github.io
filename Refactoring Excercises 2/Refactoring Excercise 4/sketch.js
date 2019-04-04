9e892f342f326dda7b5446ca0eeb49538f5b07a0
sabrinak28 <47329876+sabrinak28@users.noreply.github.com>
2019-04-04 13:43:20 -0600

    Update sketch.js

--- Refactoring Excercises 2/Refactoring Excercise 4/sketch.js
+++ Refactoring Excercises 2/Refactoring Excercise 4/sketch.js
@@ -1,36 +1,59 @@
 // Refactoring Assignment Four
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Sabrina Kettle
+// April 4th, 2019
+
+// Drawing a black and white checkerboard grid
+
+//Constants & Variables:----
 
 const WINDOW_SIZE = 600;
 const RECT_SIZE = 75;
 
 let colour = true;
 
+//---------------------------
+
+//Setup:-----------------------------------
+
 function setup() {
   createCanvas(WINDOW_SIZE, WINDOW_SIZE);
 }
 
-function blackOrWhite(){
-  if (colour){
-    fill(255);
-  }
-  else{
-    fill(0);
-  }
-  colour = !colour;
-}
+//-----------------------------------------
 
-function draw() {
-  background(220);
+//Function which draws the grid and is called from draw:----
+
+function drawGrid(){
   for (let x = 0; x < WINDOW_SIZE; x += RECT_SIZE){
     for (let y = 0; y < WINDOW_SIZE; y += RECT_SIZE){
       blackOrWhite();
       rect(x, y, RECT_SIZE, RECT_SIZE);
     }
-    colour = !colour;
+    colour = !colour; //Switch
   }
 }
+
+//-----------------------------------------------------------
+
+//Function which switches grid colours:--
+
+function blackOrWhite(){
+  if (colour){
+    fill(255); //White
+  }
+  else{
+    fill(0); //Black
+  }
+  colour = !colour; //Switch
+}
+
+//---------------------------------------
+
+//And... draw!--------
+
+function draw() {
+  background(220);
+  drawGrid();
+}
+
+//--------------------
