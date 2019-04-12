743664c4f7aee427470533fec5426fd706a05871
sabrinak28 <47329876+sabrinak28@users.noreply.github.com>
2019-04-12 13:46:34 -0600

    Update sketch.js

--- Generative Art/Art Replication/sketch.js
+++ Generative Art/Art Replication/sketch.js
@@ -1,54 +1,92 @@
 // Art Replication Warm-up
-// Your Name
-// Date
+// Sabrina Kettle
+// April 12th, 2019
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// I typically would have challenged myself more by doing one of the more difficult art pieces, but I just did this warm-up so
+// I can have more time to make my own generative art design really epic.
+
+//Variables:---------
 
 let x1, x2, y1, y2;
+const LNUM = 100;
+
+//-------------------
+
+//Setup:-------------------------------------
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noLoop();
+
+  //Assign Variables
   x1 = random(width/1.5, width/3);
   y1 = random(height/1.5, height/3);
   x2 = x1 + random(-width/6, width/6);
   y2 = y1;
+
 }
 
-  
+//---------------------------------------------
+
+//This function will draw the lines:-----------------
+
 function lines(){
 
-  for (let i = 0; i < 100; i ++){
+  for (let i = 0; i < LNUM; i ++){ //Draw 100 lines
     line(x1, y1, x2, y2);
 
+    //Connect starting position
     x1 = x2;
     y1 = y2;
 
-    if (i % 2 === 0){
-      y2 = y1;
-      if (x2 > width/2){
-        x2 = x2 + random(-width/5);
-      }
-      else{
-        x2 = x2 + random(width/5);
-      }
+    if (i % 2 === 0){ //Change direction
+      determineX();
     }
     else{
-      x2 = x1;
-      if (x2 > height){
-        y2 = y2 + random(-height/5);
-      }
-      else{
-        y2 = y2 + random(height/5);
-      }
+      determineY();
     }
   }
+
 }
+
+//----------------------------------------------------
+
+//To assign an X variable to fit the restraint:---
+
+function determineX(){
+  y2 = y1;
+  if (x2 > width/2){
+    x2 = x2 + random(-width/7);
+  }
+  else{
+    x2 = x2 + random(width/7);
+  }
+}
+
+//------------------------------------------------
+
+//To assign a Y variable to fit the restraint:----
+
+function determineY(){
+  x2 = x1;
+  if (y2 >= height - 200){
+    y2 = y2 + random(-height/4);
+  }
+  else if (y2 <= 200){
+    y2 = y2 + random(height/4);
+  }
+  else{
+    y2 = y2 + random(-height/4, height/4);
+  }
+}
+
+//-----------------------------------------------
+
+//And... draw!--------
 
 function draw() {
   background(220);
   lines();
-  
-
 }
+
+//--------------------
