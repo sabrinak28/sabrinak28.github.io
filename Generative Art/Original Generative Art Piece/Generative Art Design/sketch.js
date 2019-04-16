cd09d34e7ad63ad631882e2de4ec748e62de823e
sabrinak28 <47329876+sabrinak28@users.noreply.github.com>
2019-04-16 14:11:43 -0600

    fsafsdf

--- Generative Art/Original Generative Art Piece/Generative Art Design/sketch.js
+++ Generative Art/Original Generative Art Piece/Generative Art Design/sketch.js
@@ -6,6 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 let cHeight = 0;
+let sRed, sGreen, sBlue;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,22 +17,29 @@ function setup() {
 function circleSpiral(){
   push();
   translate(width/2, height/2);
+  sRed = 126;
+  sGreen = 107;
+  sBlue = 141;
 
-  for (let c = 1; c < 5; c++){
+  for (let c = 1; c < width; c++){
 
+    strokeWeight(2);
+    fill(sRed, sGreen, sBlue);
     ellipse(0, cHeight, width / c);
 
     for (let i = 0; i < 360; i += 6){
       push();
       rotate(radians(i));
       if (i % 12 === 0){
-        line(0, cHeight, width / c, 0);
+        strokeWeight(1);
+        line(0, cHeight, height/ c - 10, 0);
       }
       pop();
     }
 
-
-    cHeight = cHeight + width / c;
+    sRed = sRed - 8;
+    sGreen = sGreen - 8;
+    sBlue = sBlue - 8;
 
   }
 }
