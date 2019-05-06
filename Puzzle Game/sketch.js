d1b109a27db29709436f787d08e03bf41ba31433
sabrinak28 <47329876+sabrinak28@users.noreply.github.com>
2019-05-06 14:00:06 -0600

    Update sketch.js

--- Puzzle Game/sketch.js
+++ Puzzle Game/sketch.js
@@ -5,29 +5,42 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+//Data:------------------------
+
 const NUM_ROWS = 4;
 const NUM_COLS = 5;
+
 let rectWidth, rectHeight;
 let currentRow, currentCol;
 let shapeState = true;
 let win = false;
 
+
 let gridData = [[0,0,0,0,0],
-[0,0,0,0,0],
-[0,255,0,0,0],
-[255,255,255,0,0]];
+                [0,0,0,0,0],
+                [0,0,0,0,0],
+                [0,0,0,0,0]];
 
+//-----------------------------
 
+//Setup:------------------------------------------
 
 function setup() {
-  // Determine the size of each square. Could use windowHeight,windowHeight  for Canvas to keep a square aspect ratio
   createCanvas(windowWidth, windowHeight);
+
+  //Assign variables
   rectWidth = width/NUM_COLS;
   rectHeight = height/NUM_ROWS;
+
   noStroke();
 
+  //Randomize board
   startBoard();
 }
+
+//------------------------------------------------
+
+//Create random board in the beginning:--------
 
 function startBoard(){
 
@@ -44,18 +57,22 @@ function startBoard(){
   }
 }
 
+//----------------------------------------------
+
+//Flipping the tiles in a specific shaped area:--------
+
 function mousePressed(){
 
   if(mouseButton === LEFT){
-    // cross-shaped pattern flips on a mouseclick. Boundary conditions are checked within the flip function to ensure in-bounds access for array
-    if (shapeState){
+    
+    if (shapeState){ //Cross shape
       flip(currentCol, currentRow);
       flip(currentCol-1, currentRow);
       flip(currentCol+1, currentRow);
       flip(currentCol, currentRow-1);
       flip(currentCol, currentRow+1);
     }
-    else{
+    else{ //Square shape
       flip(currentCol, currentRow);
       flip(currentCol+1, currentRow);
       flip(currentCol, currentRow+1);
@@ -63,9 +80,13 @@ function mousePressed(){
     }
   }
   else if (mouseButton === RIGHT){
-    flip(currentCol, currentRow);
+    flip(currentCol, currentRow); //Cheater cheater
   }
 }
+
+//------------------------------------------------------
+
+//To change between the cross and the square:---
 
 function keyPressed(){
   if (key === " "){
@@ -73,9 +94,12 @@ function keyPressed(){
   }
 }
 
+//-----------------------------------------------
+
+//This function will flip the tiles when the mouse is pressed:
+
 function flip(col, row){
-  // given a column and row for the 2D array, flip its value from 0 to 255 or 255 to 0
-  // conditions ensure that the col and row given are valid and exist for the array. If not, no operations take place.
+  
   if (col >= 0 && col < NUM_COLS ){
     if (row >= 0 && row < NUM_ROWS){
       if (gridData[row][col] === 0){
@@ -88,21 +112,27 @@ function flip(col, row){
   }
 }
 
+//-------------------------------------------------------------
+
+//To locate which tile you are on:-----------------------------------------------------------------------------
+
 function determineActiveSquare(){
   // An expression to run each frame to determine where the mouse currently is.
   currentRow = int(mouseY / rectHeight);
   currentCol = int(mouseX / rectWidth);
 
-  if (shapeState){
-    fill(0, 255, 0, 100);
+  //Highlighting the area you will cover when the mouse is pressed
+
+  if (shapeState){ //Hightlight the cross
+    fill(0, 255, 255, 100);
     rect(currentCol * rectWidth, currentRow * rectHeight, rectWidth, rectHeight);
     rect(currentCol * rectWidth + rectWidth, currentRow * rectHeight, rectWidth, rectHeight);
     rect(currentCol * rectWidth - rectWidth, currentRow * rectHeight, rectWidth, rectHeight);
     rect(currentCol * rectWidth, currentRow * rectHeight + rectHeight, rectWidth, rectHeight);
     rect(currentCol * rectWidth, currentRow * rectHeight - rectHeight, rectWidth, rectHeight);
   }
-  else{
-    fill(0, 255, 0, 100);
+  else{ //Highlight the square
+    fill(0, 255, 255, 100);
     rect(currentCol * rectWidth, currentRow * rectHeight, rectWidth, rectHeight);
     rect(currentCol * rectWidth + rectWidth, currentRow * rectHeight, rectWidth, rectHeight);
     rect(currentCol * rectWidth, currentRow * rectHeight + rectHeight, rectWidth, rectHeight);
@@ -111,8 +141,12 @@ function determineActiveSquare(){
 
 }
 
+//------------------------------------------------------------------------------------------------------------
+
+//To render the grid:------------------------------------------
+
 function drawGrid(){
-  // Render a grid of squares - fill color set according to data stored in the 2D array
+
   for (let x = 0; x < NUM_COLS ; x++){
     for (let y = 0; y < NUM_ROWS; y++){
 
@@ -121,6 +155,10 @@ function drawGrid(){
     }
   }
 }
+
+//-------------------------------------------------------------
+
+//This function will always be playing to detect whether you won or not:----
 
 function determineWin(){
   let totalPoints = 0;
@@ -131,30 +169,47 @@ function determineWin(){
     }
   }
 
+  //If the squares are all black or the squares are all white:
   if (totalPoints === 0 || totalPoints === 255 * NUM_ROWS * NUM_COLS){
     win = true;
   }
 }
 
+//--------------------------------------------------------------------------
+
+//When you win, this snazzy screen will show:-----------
+
 function announceWin(){
-  background(225);
+  background(gridData[0][0]);
+  
+  //Fun circle border
+  for (let c = 100; c > 0; c--){
+    fill(random(255), random(255), random(255), 100);
+    ellipse(random(width), c, 600);
+    ellipse(random(width), height - c, 600);
+  }
+
+  //Text
   textSize(width/10);
   textAlign(CENTER);
   fill(228, 92, 160);
   textFont('Comic Sans MS');
   text('You win!', width/2, height/2);
-  for (let c = 100; c > 0; c--){
-    fill(random(255));
-  }
 }
+
+//------------------------------------------------------
+
+//And to play it all:-------------------------------------------------------------
 
 function draw() {
   if (win === false){
     drawGrid();  
     determineActiveSquare();   //figure out which tile the mouse cursor is over
-    determineWin();
+    determineWin(); //figure out if won or not
   }
   else{
     announceWin();
   }
 }
+
+//-------------------------------------------------------------------------------
