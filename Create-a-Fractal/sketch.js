82b799c811e95f549c1d5cb74f7c694fecaae02b
sabrinak28 <47329876+sabrinak28@users.noreply.github.com>
2019-05-13 22:36:02 -0600

    igfredgbc

--- Create-a-Fractal/sketch.js
+++ Create-a-Fractal/sketch.js
@@ -1,6 +1,6 @@
-// Project Title
-// Your Name
-// Date
+// Create-a-Fractal: "The Knot"
+// Sabrina Kettle
+// May 14th, 2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -11,9 +11,6 @@
 let angleX = 5;
 let angleY = 5;
 
-let rFill = 255;
-let gFill = 200;
-let bFill = 255;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -25,6 +22,7 @@ function links(diameter){
   if (diameter < width/100){
     rotateY(radians(angleY));
     rotateX(radians(angleX));
+
     fill(255, 0, 94);
     stroke(255, 144, 185);
     ellipsoid(diameter);
@@ -36,17 +34,16 @@ function links(diameter){
     fill(diameter/20, diameter/20, diameter/5);
     torus(diameter, diameter/15);
 
-    // rFill = rFill - 5;
-    // gFill = gFill - 5;
-    // bFill = bFill - 5;
     links(diameter*0.8);
   }
 }
 
 function draw() {
-  rotateZ(radians(frameCount/2));
   angleX = map(mouseX, 0, width, -120, 120);
   angleY = map(mouseY, 0, height, -120, 120);
+
+  rotateZ(radians(frameCount/2));
+
   background(20);
   links(width/3);
 }
