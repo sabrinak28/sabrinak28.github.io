c2bcfcf519a005a9214a08479e34fa0d58c4eb2b
sabrinak28 <47329876+sabrinak28@users.noreply.github.com>
2019-05-31 11:11:08 -0600

    fp 4

--- Capstone Project - Sabrina/sketch.js
+++ Capstone Project - Sabrina/sketch.js
@@ -27,6 +27,12 @@ let hChoice = 0;
 let iChoice = 0;
 let aChoice = 0;
 
+let wHealth = 200;
+let qHealth = 175;
+let hHealth = 300;
+let iHealth = 150;
+let aHealth = 250;
+
 let menu;
 
 let turn = 1;
@@ -63,13 +69,17 @@ class Whisper{
   constructor(x_, y_){
     this.y = y_;
     this.x = x_;
-    this.health = 200;
     this.sDamage = 50;
     this.aDamage = 100;
   }
 
   display(){
-    image(wImg[0], this.x, this.y);
+    if (wHealth > 0){
+      image(wImg[0], this.x, this.y);
+    }
+    else{
+      image(wImg[3], this.x, this.y);
+    }
   }
   
   attack(){
@@ -79,10 +89,6 @@ class Whisper{
   shoot(){
 
   }
-
-  dead(){
-
-  }
 }
 
 class Quill{
@@ -90,9 +96,9 @@ class Quill{
   constructor(x_, y_){
     this.y = y_;
     this.x = x_;
-    this.health = 200;
     this.sDamage = 50;
     this.aDamage = 100;
+    this.alive = true;
   }
 
   display(){
@@ -117,7 +123,6 @@ class Hatch{
   constructor(x_, y_){
     this.y = y_;
     this.x = x_;
-    this.health = 200;
     this.sDamage = 50;
     this.aDamage = 100;
   }
@@ -144,13 +149,12 @@ class Imellia{
   constructor(x_, y_){
     this.y = y_;
     this.x = x_;
-    this.health = 200;
     this.sDamage = 50;
     this.aDamage = 100;
   }
 
   display(){
-    image(iImg[0], this.x, this.y, 0, height/5.5);
+    image(iImg[0], this.x, this.y, 0, height/6);
   }
   
   spell(){
@@ -171,13 +175,16 @@ class Ace{
   constructor(x_, y_){
     this.y = y_;
     this.x = x_;
-    this.health = 200;
     this.sDamage = 50;
     this.aDamage = 100;
   }
 
   display(){
     image(aImg[0], this.x, this.y);
+  }
+
+  getHealth(){
+    return this.health;
   }
   
   attack(){
@@ -206,26 +213,121 @@ class Menu{
 
 function firstRound(){
 
-  if(whisper.health + quill.health + hatch.health + imellia.health + ace.health > 0){
+  if(wHealth + qHealth + hHealth + iHealth + aHealth > 0){
+
     if (turn === 1){
-      if (whisper.health > 0){
+      if (wHealth > 0){
         menu.display(0);
-        if (wChoice === 0){
-          if (keyPressed === 1){
-            wChoice === 1;
-          }
-          else if (keyPressed === 2){
-            wChoice === 2;
-          }
+
+        if (wChoice !== 0){
+          turn = 2;
+        }
+
+      }
+      else{
+        turn = 2;
+      }
+    }
+
+    if (turn === 2){
+      if (qHealth > 0){
+        menu.display(1);
+
+        if (qChoice !== 0 && qChoice !== 2){
+          turn = 3;
+        }
+
+        else if (qChoice === 2){
+          menu.display(2);
         }
       }
       else{
-        whisper.dead();
-        turn === 2;
+        turn = 3;
+      }
+    }
+
+    if (turn === 3){
+      if (hHealth > 0){
+        menu.display(3);
+
+        if (hChoice !== 0){
+          turn = 4;
+        }
+      }
+      else{
+        turn = 4;
+      }
+    }
+
+  }
+
+}
+
+function keyPressed(){
+  if (turn === 1){
+    if (key === "1"){
+      wChoice = 1;
+    }
+    if (key === "2"){
+      wChoice = 2;
+    }
+  }
+
+  if (turn === 2){
+    if (key === "1"){
+      qChoice = 1;
+    }
+    if (key === "2"){
+      qChoice = 2;
+      print(qChoice);
+    }
+
+    if (qChoice === 2){
+      if (key === "1"){
+        qChoice = 3;
+      }
+      if (key === "2"){
+        qChoice = 4;
+      }
+      if (key === "3"){
+        qChoice = 5;
+      }
+      if (key === "4"){
+        qChoice = 6;
+      }
+      if (key === "5"){
+        qChoice = 7;
       }
     }
   }
 
+
+  if (turn === 3){
+    if (key === "1"){
+      hChoice = 1;
+    }
+    if (key === "2"){
+      hChoice = 2;
+    }
+  }
+
+  if (turn === 4){
+    if (key === "1"){
+      iChoice = 1;
+    }
+    if (key === "2"){
+      iChoice = 2;
+    }
+  }
+
+  if (turn === 5){
+    if (key === "1"){
+      aChoice = 1;
+    }
+    if (key === "2"){
+      aChoice = 2;
+    }
+  }
 }
 
 function setup() {
@@ -237,20 +339,23 @@ function setup() {
   imellia = new Imellia(width/25, height/2);
   ace = new Ace(width/7.5, height/1.6);
 
+  ace.getHealth();
+
   menu = new Menu(width/7,height/1.15);
 
+  background(backgroundImg);
   whisper.display();
   quill.display();
   hatch.display();
   imellia.display();
   ace.display();
-  menu.display(0);
+
 }
 
 function draw() {
-  
+
   if (endgame === false){
-    background(backgroundImg);
+  
     firstRound();
   }
 }
